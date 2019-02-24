40161e13a53f2b66dc04431c49e374caf4c2f3a8
Frank J. Reashore <reashore@telus.net>
2019-02-23 21:19:49 -0800

    initial commit

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import Routes from "./Routes";
+
+const root: HTMLElement | null = document.getElementById("root");
+
+ReactDOM.render(<Routes />, root);
